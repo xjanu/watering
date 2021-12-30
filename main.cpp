fec0088f35018875d8f0783feab6d96d0041f3c3
Martin Janů <martin.janu@protonmail.com>
2021-12-30 17:11:09 +0100

    Moisture sensing

--- main.cpp
+++ main.cpp
@@ -5,15 +5,21 @@ const int segment_pins[8] = {6, 4, A0, 12, 11, 5, A1, 13};
 const int digit_pins[4]   = {10, 7, 8, 9};
 const int display_freq    = 300;
 
+const int moist_sensor_pin = A3;
+int moist;
+
 SegmentDisplay display{CATHODE, segment_pins, digit_pins[0],
                        digit_pins[1], digit_pins[2], digit_pins[3]};
 
 void setup() {
-  display.set_freq(display_freq);
+    display.set_freq(display_freq);
+    moist = analogRead(moist_sensor_pin);
 }
 
 void loop() {
-  unsigned now = millis() / 1000;
-  display.write_num(now % 100);
-  display.refresh();
+    if (millis() % 500 == 0) {
+        moist = (moist + analogRead(moist_sensor_pin)) / 2;
+    }
+    display.write_num(moist % 10000);
+    display.refresh();
 }
