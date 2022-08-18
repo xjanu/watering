330f4a3ab29515f29d46ddf5aaf0749caa3144c3
Martin Janů <martin.janu@protonmail.com>
2022-08-18 10:13:33 +0200

    Move pin constant definitions to a header file

---
+++ include/pins.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "Arduino.h"
+
+const int segment_pins[8] = {2, 5, 8, 10, 11, 3, 9, A5};
+const int digit_pins[4]   = {A4, 6, 7, 4};
+const int display_freq    = 300;
+
+const int moist_sensor_pin = A0;
